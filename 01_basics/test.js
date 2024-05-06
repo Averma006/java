456031f98075297055b128312c70a7bf904880a4
Averma006 <169047154+Averma006@users.noreply.github.com>
2024-05-06 13:16:18 +0000

    Datatypes

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,19 @@
-console.log("Akash")
+//console.log("Akash")
+const accountId = "234"
+let accountEmail = "Hdf@gmail.com"
+let accountPassword = "Gopikishan"
+let accountCity = "Dhanbad"
+let accountState = "Jharkhand"
+console.table ([accountCity, accountEmail, accountId, accountPassword, accountState])
+
+accountCity = "Mumbai"
+accountPassword = "234567"
+//console.table ([accountCity, accountPassword])
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
+
