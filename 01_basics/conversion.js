60af1c9751af7095d6635f1680cce493086619db
Averma006 <169047154+Averma006@users.noreply.github.com>
2024-05-07 08:32:39 +0000

    conversion

--- 01_basics/conversion.js
+++ 01_basics/conversion.js
@@ -1,5 +1,63 @@
-let score = "akash"
+let score = undefined
 
-console.log(valueInNumber) = score
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+//console.log(typeof score)
+//console.log(typeof (score))
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
+
+//Nan = Not a Number
+//Conversion 
+//33 = 33
+//"33abc" = Nan
+//true = 1
+//false = 0
+
+//let isLoggedIn = 
+//let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn)
+ 
+let someNumber = 45
+let stringNumber = String(someNumber)
+//console.log(stringNumber)
+//console.log(typeof stringNumber)
+
+//-----------------------OPERATIONS------------------------------------
+
+let value = 3
+let negValue = -value
+//console.log(negValue)
+
+//console.log(2+5);
+//console.log(2/97);
+//console.log(345/897);
+
+//console.log(1 + 3 + "4");
+//console.log("2" + "4" + 8 );
+//console.log(6789 + 234567 + "346789");
+//console.log( (3 + 5) / (6 % 3) );
+//console.log(+true)
+//console.log(+"")
+
+let gameCounter = 100
+gameCounter++
+//console.log(gameCounter);
+
+let x = 3
+const y = x++;
+//console.log(`x:${x}, y:${y}`);
+
+let a = 3
+const b = ++a;
+//console.log(`a:${a}, b:${b}`);
+
+//console.log(678 +"345" + 67398)
+//console.log(12345 - 2345)
+
+let num1 = 234
+let num2 = 567
+let num3 = "773 / 222"
+
+///console.log(num1 - num2 % num3)
+//console.log(num3)
