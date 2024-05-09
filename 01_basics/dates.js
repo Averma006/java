9334ce16bfaa6a03e19a8954c8d16aa6d71f8bb3
Averma006 <169047154+Averma006@users.noreply.github.com>
2024-05-09 14:12:44 +0000

    commit message

---
+++ 01_basics/dates.js
@@ -0,0 +1,18 @@
+let myDate = new Date()
+//console.log(myDate.toLocaleDateString())
+///console.log(myDate.toLocaleTimeString())
+
+//console.log(typeof myDate)
+
+let calendar = new Date(2024, 6, 6, 5, 9)
+//console.log(calendar.toDateString())
+//console.log(calendar.toLocaleString())
+
+//console.log(calendar.getTime())
+//console.log(calendar.getTime())
+
+//console.log(Math.floor(Date.now()))
+
+
+const newDate = new Date(2024, 03, 4, 3, 4)
+console.log(newDate.toLocaleString())
