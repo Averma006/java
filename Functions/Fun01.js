d6880becfad5c82a7f23005492b8cea4aadcfd0a
Averma006 <169047154+Averma006@users.noreply.github.com>
2024-06-09 14:48:52 +0000

    commit message

---
+++ Functions/Fun01.js
@@ -0,0 +1,12 @@
+function addTwoNumbers(number1, Number2){
+    console.log(number1 + Number2);
+
+}
+const result = addTwoNumbers(456789,490909)
+
+console.log("Result:", result);
+
+return result
+
+
+
